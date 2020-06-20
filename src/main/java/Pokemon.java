8f82e1f8c76e371cca3d2c06b35e4917a19cd642
messynumber08 <32477103+messynumber08@users.noreply.github.com>
2020-06-20 20:47:46 +0800

    translate

--- src/main/java/Pokemon.java
+++ src/main/java/Pokemon.java
@@ -1,7 +1,6 @@
 
 import java.util.Random;
 
-
 public class Pokemon {
   private String name = null;
   private int strength = 1;
@@ -11,19 +10,19 @@ public class Pokemon {
 
   public Pokemon(String name, int strength, int health, int level, int exp) throws Exception {
     if (name == null)
-      throw new Exception("名字不可為空");
+      throw new Exception("name cannot be empty");
 
     if (strength < 1)
-      throw new Exception("力量不可小於1");
+      throw new Exception("strength cannot be less than 1");
 
     if (health < 1)
-      throw new Exception("體力不可小於1");
+      throw new Exception("health cannot be less than 1");
 
     if (level < 1)
-      throw new Exception("等級不可小於1");
+      throw new Exception("level cannot be less than 1");
 
     if (exp < 0)
-      throw new Exception("經驗不可小於0");
+      throw new Exception("exp  cannot be less than 0");
 
     this.name = name;
     this.strength = strength;
@@ -37,30 +36,30 @@ public class Pokemon {
     StringBuilder result = new StringBuilder();
     int playerHealth = this.health;
     int enemyHealth = enemy.health;
-    result.append(this.name + "對" + enemy.name + "發起了挑戰");
+    result.append(this.name + " challenged " + enemy.name);
     boolean flag = true;
     do {
       int playerStrength = random.nextInt(this.strength) + (int) (this.strength * 0.2);
       enemyHealth -= playerStrength;
-      result.append("\n" + this.name + "攻擊," + enemy.name + "受到" + playerStrength + "點傷害");
+      result.append("\n" + this.name + " attack," + enemy.name + " takes " + playerStrength + " damage");
 
       if (enemyHealth <= 0)
         break;
 
       int enemyStrength = random.nextInt(enemy.strength) + (int) (enemy.strength * 0.4);
       playerHealth -= enemyStrength;
-      result.append("\n" + enemy.name + "攻擊," + this.name + "受到" + enemyStrength + "點傷害");
+      result.append("\n" + enemy.name + " attack," + this.name + " takes " + enemyStrength + " damage");
 
       if (playerHealth <= 0)
         flag = false;
     } while (flag);
 
     if (playerHealth < 1) {
-      result.append("\n" + this.name + "戰敗了," + enemy.name + "還有" + enemyHealth + "點血量");
+      result.append("\n" + this.name + " defeated," + enemy.name + " has " + enemyHealth + " HP");
       result.append("\n" + this.gainExp(9));
     }
     if (enemyHealth < 1) {
-      result.append("\n" + this.name + "勝利了," + this.name + "還有" + playerHealth + "點血量");
+      result.append("\n" + this.name + " won," + this.name + " has " + playerHealth + " HP");
       result.append("\n" + this.gainExp(36));
     }
     return result.toString();
@@ -69,17 +68,17 @@ public class Pokemon {
   public String getMyRole() {
     StringBuilder result = new StringBuilder();
 
-    result.append("名稱:" + this.name + "\n");
-    result.append("攻擊:" + this.strength + "\n");
-    result.append("血量:" + this.health + "\n");
-    result.append("等級:" + this.level + "\n");
-    result.append("經驗值:" + this.exp);
+    result.append("name:" + this.name + "\n");
+    result.append("strength:" + this.strength + "\n");
+    result.append("health:" + this.health + "\n");
+    result.append("level:" + this.level + "\n");
+    result.append("exp:" + this.exp);
     return result.toString();
   }
 
   public String gainExp(int exp) {
     StringBuilder result = new StringBuilder();
-    result.append("獲得了" + exp + "點經驗");
+    result.append("Gained " + exp + " experience");
     this.exp += exp;
     return result.toString();
   }
--- src/test/java/PokemonTest.java
+++ src/test/java/PokemonTest.java
@@ -2,7 +2,6 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-
 public class PokemonTest {
   private Pokemon player, enemy;
 
@@ -43,9 +42,10 @@ public class PokemonTest {
 
   @Test
   public void playerWin() throws Exception {
-    String result = "player對enemy發起了挑戰\n" + "player攻擊,enemy受到37點傷害\n" + "enemy攻擊,player受到22點傷害\n"
-        + "player攻擊,enemy受到11點傷害\n" + "enemy攻擊,player受到30點傷害\n" + "player攻擊,enemy受到8點傷害\n" + "player勝利了,player還有1點血量\n"
-        + "獲得了36點經驗";
+    String result = "player challenged enemy\n" + "player attack,enemy takes 37 damage\n"
+        + "enemy attack,player takes 22 damage\n" + "player attack,enemy takes 11 damage\n"
+        + "enemy attack,player takes 30 damage\n" + "player attack,enemy takes 8 damage\n"
+        + "player won,player has 1 HP\n" + "Gained 36 experience";
     player = new Pokemon("player", 35, 53, 1, 0);
     enemy = new Pokemon("enemy", 30, 50, 1, 0);
     assertEquals(result, player.battle(enemy, 100));
@@ -53,8 +53,9 @@ public class PokemonTest {
 
   @Test
   public void playerLose() throws Exception {
-    String result = "player對enemy發起了挑戰\n" + "player攻擊,enemy受到31點傷害\n" + "enemy攻擊,player受到22點傷害\n"
-        + "player攻擊,enemy受到10點傷害\n" + "enemy攻擊,player受到30點傷害\n" + "player戰敗了,enemy還有10點血量\n" + "獲得了9點經驗";
+    String result = "player challenged enemy\n" + "player attack,enemy takes 31 damage\n"
+        + "enemy attack,player takes 22 damage\n" + "player attack,enemy takes 10 damage\n"
+        + "enemy attack,player takes 30 damage\n" + "player defeated,enemy has 10 HP\n" + "Gained 9 experience";
     player = new Pokemon("player", 30, 45, 1, 0);
     enemy = new Pokemon("enemy", 30, 51, 1, 0);
     assertEquals(result, player.battle(enemy, 100));
@@ -62,14 +63,14 @@ public class PokemonTest {
 
   @Test
   public void getMyRole() throws Exception {
-    String result = "名稱:player\n攻擊:1\n血量:50\n等級:1\n經驗值:0";
+    String result = "name:player\nstrength:1\nhealth:50\nlevel:1\nexp:0";
     player = new Pokemon("player", 1, 50, 1, 0);
     assertEquals(result, player.getMyRole());
   }
 
   @Test
   public void gainExp() throws Exception {
-    String result = "獲得了18點經驗";
+    String result = "Gained 18 experience";
     player = new Pokemon("player", 1, 50, 1, 0);
     assertEquals(result, player.gainExp(18));
     assertEquals(18, player.getExp());
@@ -77,7 +78,7 @@ public class PokemonTest {
 
   @Test
   public void levelUp() throws Exception {
-    String result = "名稱:player\n攻擊:6\n血量:65\n等級:2\n經驗值:-33\nLevel up";
+    String result = "name:player\nstrength:6\nhealth:65\nlevel:2\nexp:-33\nLevel up";
     player = new Pokemon("player", 1, 50, 1, 0);
     assertEquals(result, player.levelUp());
     assertEquals("player", player.getName());
